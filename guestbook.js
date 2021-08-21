a8af5aa8a4aea24577789665fb55ab2ef4be93cc
IBM <49950247+orchestrationAPI@users.noreply.github.com>
2021-08-21 23:25:13 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '/entries';
+const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/a98dedfaefd07bacbc1449d422da0b32cccb92dbb8bc8d1f5c739e01cf9e6fe2/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
